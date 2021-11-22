677f154e391b06e169bd4508741a716e886595a1
김민성 <kmsz5813@naver.com>
2021-11-22 12:12:29 +0900

    대댓글 목록 표시  - Bug : 좋아요 /싫어요 앱 강제종료

--- app/src/main/java/com/neppplus/colosseum_20211117/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/neppplus/colosseum_20211117/ViewReplyDetailActivity.kt
@@ -3,14 +3,20 @@ package com.neppplus.colosseum_20211117
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.neppplus.colosseum_20211117.adapters.ReReplyAdapter
 import com.neppplus.colosseum_20211117.databinding.ActivityViewReplyDetailBinding
 import com.neppplus.colosseum_20211117.datas.ReplyData
+import com.neppplus.colosseum_20211117.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewReplyDetailActivity : BaseActivity() {
 
     lateinit var binding : ActivityViewReplyDetailBinding
 
     lateinit var mReplyData: ReplyData
+
+    val mReReplyList = ArrayList<ReReplyAdapter>()
+    lateinit var mReReplyAdapter : ReReplyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +36,46 @@ class ViewReplyDetailActivity : BaseActivity() {
         binding.txtWriterNickname.text = mReplyData.writer.nickname
 
         binding.txtContent.text = mReplyData.content
+
+        getReplyDetailFromServer()
+
+        mReReplyAdapter = ReReplyAdapter(mContext,R.layout.re_reply_list_item, mReReplyList)
+        binding.reReplyListView.adapter = mReReplyAdapter
+
+    }
+
+    fun getReplyDetailFromServer() {
+
+        ServerUtil.getRequestReplyDetail(mContext, mReplyData.id, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val replyObj = dataObj.getJSONObject("reply")
+                val repliesArr = replyObj.getJSONArray("replies")
+
+                mReReplyList.clear()
+
+                for ( i in 0 until repliesArr.length() ){
+
+//                    [] = {}  (JSONObject) 추출 -> ReplyData로 변환 -> 대댓글 목록에 추가 (최종목표)
+
+                    mReReplyList.add(ReplyData.getReplyDataFromJson(  repliesArr.getJSONObject(i)))
+
+                }
+
+
+                runOnUiThread {
+
+                    mReReplyAdapter.notifyDataSetChanged()
+
+
+                }
+
+
+            }
+
+
+        })
 
     }
 
